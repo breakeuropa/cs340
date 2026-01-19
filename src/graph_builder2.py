46bcb46d97a22d8751c10c49140fa7308a20f4b7
kawalls <ktwalls04@gmail.com>
2026-01-18 19:26:46 -0600

    3rd commit. Graph class template is added.

---
+++ src/graph_builder2.py
@@ -0,0 +1,20 @@
+class Graph:
+
+    def __init__(self):
+        pass
+
+    def add_node(self):
+        pass
+
+    def remove_node(self):
+        pass
+
+    def remove_edit(self):
+        pass
+
+    def _adjacency_list(self):
+        pass
+
+
+if __name__ == "__main__":
+    print("Hello world")

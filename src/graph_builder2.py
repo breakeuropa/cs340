4914a70d21adc305d7ef229a2b988506cb28837d
kawalls <ktwalls04@gmail.com>
2026-01-20 21:44:05 -0600

    +remove_node

--- src/graph_builder2.py
+++ src/graph_builder2.py
@@ -21,8 +21,16 @@ class Graph:
 
         self._nodes[node_src][node_dst] = cost
 
-    # def remove_node(self):
-    #     pass
+    def remove_node(self, node: str) -> None:
+
+        if not node in self._nodes.keys():
+            return
+        
+        for key in self._nodes.keys():
+            if node in self._nodes[key]:
+                self._nodes[key].pop(node)
+        
+        self._nodes.pop(node)
 
     def remove_edge(self, src_node: str, node: str):
 
@@ -44,6 +52,10 @@ class Graph:
                 txt += f"{pair[0]}({pair[1]}) "
             
             print(f"{txt}")
+
+    def update_cost(self, src_node: str, dst_node: str, cost: int) -> None:
+        self.add_edge(src_node, dst_node, cost)
+
 
 def load_file(srcfile: str) -> Graph:
     new_graph = Graph()
@@ -73,20 +85,42 @@ if __name__ == "__main__":
 
     # Update 2; added remove_edge:
 
+    # area.add_node("City1")
+    # area.add_node("City2")
+    # area.add_node("City3")
+
+    # area.add_edge("City1", "City2", 4)
+    # area.add_edge("City2", "City3", 8)
+
+    # print(f"{area._nodes}")
+
+    # print(f"\nBefore removing City2 from City1:")
+    # area._adjacency_list()
+
+    
+
+    # # area.remove_edge("City1", "City2")
+    # area.update_cost("City2", "City3", 40)
+
+
+    # print(f"\nAfter removing City2 from City1:")
+    # area._adjacency_list()
+
+    # Update 3; added remove_node:
+
     area.add_node("City1")
     area.add_node("City2")
     area.add_node("City3")
 
     area.add_edge("City1", "City2", 4)
     area.add_edge("City2", "City3", 8)
+    area.add_edge("City3", "City2", 2)
+    area.add_edge("City3", "City1", 14)
 
-    print(f"{area._nodes}")
-
-    print(f"\nBefore removing City2 from City1:")
+    print(f"\nBefore removing City2")
     area._adjacency_list()
 
-    area.remove_edge("City1", "City2")
+    area.remove_node("City2")
 
-
-    print(f"\nAfter removing City2 from City1:")
+    print(f"\nAfter removing City2")
     area._adjacency_list()

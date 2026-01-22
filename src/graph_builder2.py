e22b70cc3913f4ef0c68956af783a2549a627618
kawalls <ktwalls04@gmail.com>
2026-01-22 08:18:48 -0600

    + cmd arguments, +try-block err prev

--- src/graph_builder2.py
+++ src/graph_builder2.py
@@ -1,3 +1,5 @@
+import sys
+
 class Graph:
 
     _nodes: dict
@@ -56,9 +58,6 @@ class Graph:
             
             print(f"{txt}")
 
-    def update_cost(self, src_node: str, dst_node: str, cost: int) -> None:
-        self.add_edge(src_node, dst_node, cost)
-
     def get_adjacent_nodes(self, node: str) -> dict:
         if not node in self._nodes:
             return {}
@@ -75,76 +74,112 @@ class Graph:
             return -1
         
         return self._nodes[src_node][dst_node]
+    
+    def set_cost(self, src_node: str, dst_node: str, cost: int) -> None:
+        # the case: if it doesn't exist in main dict or node dict, don't do anything.
+        # I should make these errors lol
+        if not src_node in self._nodes.keys():
+            return 
+        
+        # if not 
 
+        self.add_edge(src_node, dst_node, cost)
 
 def load_file(srcfile: str) -> Graph:
     new_graph = Graph()
 
+    try:
+        file = open(srcfile, "r")
+    except:
+        print(f"Error opening {srcfile}")
+        return
+    
+    for line in file:
+
+        if line.startswith('CITIES'):
+            for line in file:
+
+                if line.startswith('ROADS'):
+                    return
+                
+                print(f"{line}", end="")
+
     return new_graph
 
+def main(argv: list) -> None:
 
+    if len(argv) != 2:
+        print(f"Invalid. Command usage: python {argv[0]} <src file>")
+        return
+    
+    city_graph: Graph = load_file(argv[1])
 
-
+# Update 5; argument pass
 if __name__ == "__main__":
-    print("Hello world")
+    main(sys.argv)
+    
 
-    area = Graph()
+# if __name__ == "__main__":
+#     print("Hello world")
 
-    # Update 1; added add_node, add_edge, adjacency_list
+#     area = Graph()
 
-    # area.add_node("City1")
-    # area.add_node("City2")
-    # area.add_node("City3")
-    # area.add_node("City4")
+#     # Update 1; added add_node, add_edge, adjacency_list
 
-    # area.add_edge("City1", "City2", 4)
-    # area.add_edge("City1", "City4", 10)
-    # area.add_edge("City3", "City1", 5)
-    # area.add_edge("City2", "City3", 2)
-    # area.add_edge("City1", "City4", 15)
+#     # area.add_node("City1")
+#     # area.add_node("City2")
+#     # area.add_node("City3")
+#     # area.add_node("City4")
 
-    # Update 2; added remove_edge:
+#     # area.add_edge("City1", "City2", 4)
+#     # area.add_edge("City1", "City4", 10)
+#     # area.add_edge("City3", "City1", 5)
+#     # area.add_edge("City2", "City3", 2)
+#     # area.add_edge("City1", "City4", 15)
 
-    # area.add_node("City1")
-    # area.add_node("City2")
-    # area.add_node("City3")
+#     # Update 2; added remove_edge:
 
-    # area.add_edge("City1", "City2", 4)
-    # area.add_edge("City2", "City3", 8)
+#     # area.add_node("City1")
+#     # area.add_node("City2")
+#     # area.add_node("City3")
 
-    # print(f"{area._nodes}")
+#     # area.add_edge("City1", "City2", 4)
+#     # area.add_edge("City2", "City3", 8)
 
-    # print(f"\nBefore removing City2 from City1:")
-    # area._adjacency_list()
+#     # print(f"{area._nodes}")
+
+#     # print(f"\nBefore removing City2 from City1:")
+#     # area._adjacency_list()
 
     
 
-    # # area.remove_edge("City1", "City2")
-    # area.update_cost("City2", "City3", 40)
+#     # # area.remove_edge("City1", "City2")
+#     # area.update_cost("City2", "City3", 40)
 
 
-    # print(f"\nAfter removing City2 from City1:")
-    # area._adjacency_list()
+#     # print(f"\nAfter removing City2 from City1:")
+#     # area._adjacency_list()
 
-    # Update 3; added remove_node:
+#     # Update 3; added remove_node:
 
-    area.add_node("City1")
-    area.add_node("City2")
-    area.add_node("City3")
+#     area.add_node("City1")
+#     area.add_node("City2")
+#     area.add_node("City3")
 
-    area.add_edge("City1", "City2", 4)
-    area.add_edge("City2", "City3", 8)
-    area.add_edge("City3", "City2", 2)
-    area.add_edge("City3", "City1", 14)
+#     area.add_edge("City1", "City2", 4)
+#     area.add_edge("City2", "City3", 8)
+#     area.add_edge("City3", "City2", 2)
+#     area.add_edge("City3", "City1", 14)
 
-    # print(f"\nBefore removing City2")
-    # area._adjacency_list()
+#     # print(f"\nBefore removing City2")
+#     # area._adjacency_list()
 
-    # area.update_cost("City5", "City6", 10)
+#     # area.set_cost("City5", "City6", 10)
 
-    print(f"{area.get_cost('City3', 'City1')}")
+#     print(f"{area.get_cost('City3', 'City1')}")
+#     area.set_cost()
 
-    # area.remove_node("City2")
+#     # area.remove_node("City2")
 
-    # print(f"\nAfter removing City2")
-    # area._adjacency_list()
+#     # print(f"\nAfter removing City2")
+#     # area._adjacency_list()
